8f7eade817b1b5859daea8d22bc07bbc58240d6d
David Sanchez <dsanctech@gmail.com>
2019-02-28 10:33:34 -0500

    Launch Screen

---
+++ iTunes Viewer/LaunchScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchScreenViewController.swift
+//  iTunes Viewer
+//
+//  Created by David Sanchez on 2/28/19.
+//  Copyright © 2019 DSancTech. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
