edd9be5c054d144121ba4bd3fbc2d04320f0e8d2
David Sanchez <dsanctech@gmail.com>
2019-02-28 12:26:36 -0500

    Launch Screen completed.

--- iTunes Viewer/LaunchScreenViewController.swift
+++ iTunes Viewer/LaunchScreenViewController.swift
@@ -9,22 +9,51 @@
 import UIKit
 
 class LaunchScreenViewController: UIViewController {
+    
+    // BG Image
+    var bgImage: UIImage?
+    var bgImageView: UIImageView?
+    
+    // Launch Icon Image
+    var launchIconImage: UIImage?
+    var launchIconImageView: UIImageView?
 
+    // View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Set background color:
+        self.view.backgroundColor = UIColor.init(red:0.92, green:0.59, blue:0.16, alpha:1.0)
+        
+        // Set background image:
+        bgImage = UIImage.init(named: "LaunchGradientBG")
+        bgImageView = UIImageView(frame: self.view.bounds)
+        bgImageView?.contentMode = .scaleToFill
+        bgImageView?.center = self.view.center
+        bgImageView?.image = bgImage
+        self.view.addSubview(bgImageView ?? UIView.init(frame: self.view.bounds))
+        self.view.sendSubviewToBack(bgImageView ?? UIView.init(frame: self.view.bounds))
+        
+        // Set launch icon image:
+        launchIconImage = UIImage.init(named: "LaunchIcon")
+        launchIconImageView = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0))
+        launchIconImageView?.contentMode = .scaleAspectFit
+        launchIconImageView?.center = self.view.center
+        launchIconImageView?.image = launchIconImage
+        self.view.addSubview(launchIconImageView ?? UIView.init(frame: self.view.bounds))
+        self.view.bringSubviewToFront(launchIconImageView ?? UIView.init(frame: self.view.bounds))
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // View Did Appear
+    override func viewDidAppear(_ animated: Bool) {
+        
     }
-    */
+    
+    // Set status bar style.
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
 
 }
