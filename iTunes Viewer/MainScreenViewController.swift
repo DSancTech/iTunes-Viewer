959789164422d72ffc251b9b54149fd609cd1c1f
David Sanchez <dsanctech@gmail.com>
2019-03-01 11:26:49 -0500

    Picker view completed.

--- iTunes Viewer/MainScreenViewController.swift
+++ iTunes Viewer/MainScreenViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainScreenViewController: UIViewController {
+class MainScreenViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     // Top Container View
     var topContainerView = UIView()
@@ -102,17 +102,51 @@ class MainScreenViewController: UIViewController {
     var bottomContainerViewBottomConstraint = NSLayoutConstraint()
     var bottomContainerViewTopConstraint = NSLayoutConstraint()
     
+    // Pop Up Darken View
+    var popUpDarkenView = UIView()
+    
+    // Pop Up View
+    var popUpView = UIView()
+    var popUpViewWidthConstraint = NSLayoutConstraint()
+    var popUpViewHeightConstraint = NSLayoutConstraint()
+    var popUpViewXConstraint = NSLayoutConstraint()
+    var popUpViewYConstraint = NSLayoutConstraint()
+    
+    // Picker Container View
+    var pickerContainerView = UIView()
+    var pickerView = UIPickerView()
+    var pickerContainerViewLeftConstraint = NSLayoutConstraint()
+    var pickerContainerViewRightConstraint = NSLayoutConstraint()
+    var pickerContainerViewBottomConstraint = NSLayoutConstraint()
+    var pickerContainerViewTopConstraint = NSLayoutConstraint()
+    
+    // Picker Done Button Container View
+    var pickerDoneButton = UIButton()
+    var pickerDoneButtonBottomConstraint = NSLayoutConstraint()
+    var pickerDoneButtonLeftConstraint = NSLayoutConstraint()
+    var pickerDoneButtonRightConstraint = NSLayoutConstraint()
+    var pickerDoneButtonHeightConstraint = NSLayoutConstraint()
+    
     // View variables
     var topSafeArea = CGFloat()
     var bottomSafeArea = CGFloat()
     var viewHeight = CGFloat()
     var viewWidth = CGFloat()
     
+    // Selected Picker
+    var selectedPicker = 0
+    
     // Arrays
     var selectedMediaTypeArray: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     var selectedMediaTypeTextArray: [String] = ["Apple Music", "iTunes Music", "iOS Apps", "Mac Apps", "Audiobooks", "Books", "TV Shows", "Movies", "iTunes U", "Podcasts", "Music Videos"]
+    var selectedMediaType = 0
     var selectedFeedTypeArray: [[Int]] = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14, 15, 16], [17, 18, 19, 20], [21], [22, 23], [24, 25], [26], [27], [28], [29]]
-    var selectedFeedTypeTextArray: [String] = ["Coming Soon", "Hot Tracks", "New Releases", "Top Albums", "Top Songs", "Hot Tracks", "New Music", "Recent Releases", "Top Albums", "Top Songs", "New Apps We Love", "New Games We Love", "Top Free", "Top Free iPad", "Top Grossing", "Top Grossing Paid", "Top Paid", "Top Free Mac Apps", "Top Grossing Mac Apps", "Top Mac Apps", "Top Paid Mac Apps", "Top Audiobooks", "Top Free", "Top Paid", "Top TV Episodes", "Top TV Seasons", "Top Movies", "Top iTunes U Courses", "Top Podcasts", "Top Music Videos",]
+    var selectedFeedTypeTextArray: [String] = ["Coming Soon", "Hot Tracks", "New Releases", "Top Albums", "Top Songs", "Hot Tracks", "New Music", "Recent Releases", "Top Albums", "Top Songs", "New Apps We Love", "New Games We Love", "Top Free", "Top Free iPad", "Top Grossing", "Top Grossing Paid", "Top Paid", "Top Free Mac Apps", "Top Grossing Mac Apps", "Top Mac Apps", "Top Paid Mac Apps", "Top Audiobooks", "Top Free", "Top Paid", "Top TV Episodes", "Top TV Seasons", "Top Movies", "Top iTunes U Courses", "Top Podcasts", "Top Music Videos"]
+    var selectedFeedType = 0
+    var selectedFeedInRow = 0
+    
+    // Table View Data Source URL String
+    var tableViewDataSourceURLString: String = "https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/10/explicit.json"
     
     // View Did Load
     override func viewDidLoad() {
@@ -161,7 +195,7 @@ class MainScreenViewController: UIViewController {
         
         // Set up media type container view:
         mediaTypeContainerView.backgroundColor = UIColor.white.withAlphaComponent(0.15)
-        mediaTypeContainerView.layer.cornerRadius = 10.0
+        mediaTypeContainerView.layer.cornerRadius = 15.0
         mediaTypeContainerView.clipsToBounds = true
         mediaTypeContainerView.translatesAutoresizingMaskIntoConstraints = false
         self.topContainerView.addSubview(mediaTypeContainerView)
@@ -265,7 +299,7 @@ class MainScreenViewController: UIViewController {
         
         // Set up feed type container view:
         feedTypeContainerView.backgroundColor = UIColor.white.withAlphaComponent(0.15)
-        feedTypeContainerView.layer.cornerRadius = 10.0
+        feedTypeContainerView.layer.cornerRadius = 15.0
         feedTypeContainerView.clipsToBounds = true
         feedTypeContainerView.translatesAutoresizingMaskIntoConstraints = false
         self.topContainerView.addSubview(feedTypeContainerView)
@@ -371,6 +405,73 @@ class MainScreenViewController: UIViewController {
         bottomContainerViewTopConstraint.isActive = true
         bottomContainerViewBottomConstraint.isActive = true
         
+        // Set up pop up darken view:
+        popUpDarkenView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        popUpDarkenView.alpha = 0.0
+        popUpDarkenView.frame = self.view.frame
+        popUpDarkenView.center = self.view.center
+        self.view.addSubview(popUpDarkenView)
+        self.view.bringSubviewToFront(popUpDarkenView)
+        
+        // Set up pop up view:
+        popUpView.backgroundColor = UIColor(red: 0.93, green: 0.94, blue: 0.96, alpha: 1.0)
+        popUpView.layer.cornerRadius = 25.0
+        popUpView.clipsToBounds = true
+        popUpView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(popUpView)
+        self.view.bringSubviewToFront(popUpView)
+        popUpViewWidthConstraint = popUpView.widthAnchor.constraint(equalToConstant: (viewWidth - 48.0))
+        popUpViewHeightConstraint = popUpView.heightAnchor.constraint(equalToConstant: (viewWidth - 48.0))
+        popUpViewXConstraint = popUpView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0.0)
+        popUpViewYConstraint = popUpView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0.0)
+        popUpViewWidthConstraint.isActive = true
+        popUpViewHeightConstraint.isActive = true
+        popUpViewXConstraint.isActive = true
+        popUpViewYConstraint.isActive = true
+        popUpView.transform = CGAffineTransform.identity.scaledBy(x: 0.0, y: 0.0)
+        
+        // Set up picker container view:
+        pickerContainerView.translatesAutoresizingMaskIntoConstraints = false
+        pickerContainerView.clipsToBounds = true
+        self.popUpView.addSubview(pickerContainerView)
+        pickerContainerViewLeftConstraint = pickerContainerView.leadingAnchor.constraint(equalTo: popUpView.leadingAnchor, constant: 0.0)
+        pickerContainerViewRightConstraint = pickerContainerView.trailingAnchor.constraint(equalTo: popUpView.trailingAnchor, constant: 0.0)
+        pickerContainerViewTopConstraint = pickerContainerView.topAnchor.constraint(equalTo: popUpView.topAnchor, constant: 0.0)
+        pickerContainerViewBottomConstraint = pickerContainerView.bottomAnchor.constraint(equalTo: popUpView.bottomAnchor, constant: 60.0)
+        pickerContainerViewLeftConstraint.isActive = true
+        pickerContainerViewRightConstraint.isActive = true
+        pickerContainerViewBottomConstraint.isActive = true
+        pickerContainerViewTopConstraint.isActive = true
+        
+        // Set up picker done button:
+        pickerDoneButton.setTitle("", for: .normal)
+        pickerDoneButton.addTarget(self, action: #selector(pickerDoneButtonAction), for: .touchUpInside)
+        pickerDoneButton.setBackgroundImage(UIImage(named: "LaunchGradientBG"), for: .normal)
+        pickerDoneButton.setTitle("Done", for: .normal)
+        pickerDoneButton.setTitleColor(UIColor.white, for: .normal)
+        pickerDoneButton.translatesAutoresizingMaskIntoConstraints = false
+        self.popUpView.addSubview(pickerDoneButton)
+        self.popUpView.bringSubviewToFront(pickerDoneButton)
+        pickerDoneButtonLeftConstraint = pickerDoneButton.leadingAnchor.constraint(equalTo: popUpView.leadingAnchor, constant: 0.0)
+        pickerDoneButtonRightConstraint = pickerDoneButton.trailingAnchor.constraint(equalTo: popUpView.trailingAnchor, constant: 0.0)
+        pickerDoneButtonBottomConstraint = pickerDoneButton.bottomAnchor.constraint(equalTo: popUpView.bottomAnchor, constant: 0.0)
+        pickerDoneButtonHeightConstraint = pickerDoneButton.heightAnchor.constraint(equalToConstant: 60.0)
+        pickerDoneButtonLeftConstraint.isActive = true
+        pickerDoneButtonRightConstraint.isActive = true
+        pickerDoneButtonBottomConstraint.isActive = true
+        pickerDoneButtonHeightConstraint.isActive = true
+        
+        // Set up picker view:
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.frame.size.height = (viewWidth - 48.0) - 60.0
+        self.pickerContainerView.addSubview(pickerView)
+        
+    }
+    
+    // View did appear
+    override func viewDidAppear(_ animated: Bool) {
+        updateURL()
     }
     
     // View did layout subviews
@@ -408,17 +509,131 @@ class MainScreenViewController: UIViewController {
     
     // Media Type Button Action
     @objc func mediaTypeButtonAction(sender: UIButton!) {
-        print("Media Type Button Pressed.")
+        if (selectedPicker != 0) {
+            selectedPicker = 0
+            pickerView.reloadAllComponents()
+            pickerView.selectRow(selectedMediaType, inComponent: 0, animated: false)
+        }
+        UIView.animate(
+            withDuration: 0.15,
+            delay: 0.0,
+            options: .curveEaseInOut,
+            animations: {
+                self.popUpDarkenView.alpha = 1.0
+        }, completion: nil)
+        UIView.animate(
+            withDuration: 0.45,
+            delay: 0.0,
+            usingSpringWithDamping: 0.8,
+            initialSpringVelocity: 0.0,
+            animations: {
+                self.popUpView.transform = CGAffineTransform.identity
+        },  completion: nil)
     }
     
     // Feed Type Button Action
     @objc func feedTypeButtonAction(sender: UIButton!) {
-        print("Feed Type Button Pressed.")
+        if (selectedPicker != 1) {
+            selectedPicker = 1
+            pickerView.reloadAllComponents()
+            pickerView.selectRow(selectedFeedInRow, inComponent: 0, animated: false)
+        }
+        UIView.animate(
+            withDuration: 0.15,
+            delay: 0.0,
+            options: .curveEaseInOut,
+            animations: {
+                self.popUpDarkenView.alpha = 1.0
+        }, completion: nil)
+        UIView.animate(
+            withDuration: 0.45,
+            delay: 0.0,
+            usingSpringWithDamping: 0.8,
+            initialSpringVelocity: 0.0,
+            animations: {
+                self.popUpView.transform = CGAffineTransform.identity
+        },  completion: nil)
+        
+    }
+    
+    // Picker Done Button Action
+    @objc func pickerDoneButtonAction(sender: UIButton!) {
+        UIView.animate(
+            withDuration: 0.15,
+            delay: 0.0,
+            options: .curveEaseInOut,
+            animations: {
+                self.popUpDarkenView.alpha = 0.0
+                self.popUpView.transform = CGAffineTransform.identity.scaledBy(x: 0.001, y: 0.001)
+        }, completion: nil)
+        updateURL()
+    }
+    
+    // Picker view number of components
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Picker view number of rows
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (selectedPicker == 0) {
+            return selectedMediaTypeTextArray.count
+        }
+        else {
+            return selectedFeedTypeArray[selectedMediaType].count
+        }
+    }
+    
+    // Picker view row height
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 50.0
+    }
+    
+    // Picker view view for row:
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        var titleLabel: UILabel? = (view as? UILabel)
+        if titleLabel == nil {
+            titleLabel = UILabel()
+            titleLabel?.font = UIFont.systemFont(ofSize: 18.0, weight: .regular)
+            titleLabel?.textAlignment = .center
+        }
+        titleLabel?.textColor = UIColor.black
+        
+        if (selectedPicker == 0) {
+            titleLabel?.text = selectedMediaTypeTextArray[row]
+        }
+        else {
+            titleLabel?.text = selectedFeedTypeTextArray[selectedFeedTypeArray[selectedMediaType][row]]
+        }
+        
+        return titleLabel!
+    }
+    
+    // Picker view did select row:
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (selectedPicker == 0) {
+            selectedMediaType = row
+            selectedFeedInRow = 0
+            mediaTypeLabel.text = selectedMediaTypeTextArray[selectedMediaType]
+            feedTypeLabel.text = selectedFeedTypeTextArray[selectedFeedTypeArray[selectedMediaType][0]]
+        }
+        else {
+            selectedFeedType = selectedFeedTypeArray[selectedMediaType][row]
+            selectedFeedInRow = row
+            feedTypeLabel.text = selectedFeedTypeTextArray[selectedFeedType]
+        }
     }
     
     // Set status bar style.
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-
+    
+    // Update URL
+    func updateURL() {
+        if (mediaTypeLabel.text != nil && feedTypeLabel.text != nil) {
+            tableViewDataSourceURLString = "https://rss.itunes.apple.com/api/v1/us/" + (mediaTypeLabel.text?.lowercased().replacingOccurrences(of: " ", with: "-"))! + "/" + (feedTypeLabel.text?.lowercased().replacingOccurrences(of: " ", with: "-"))! + "/all/10/explicit.json"
+        }
+    }
+ 
 }
